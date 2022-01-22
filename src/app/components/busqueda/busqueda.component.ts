7cc7e61727321cd11e5359d81069492f1741581e
Jesus Garcia <yisusgarcia1002@gmail.com>
2022-01-22 15:25:51 -0500

    Se creo el componete busqueda para mostrar la lista de resultados obtenidos

---
+++ src/app/components/busqueda/busqueda.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { ActivatedRoute } from '@angular/router';
+
+////////////////////////////////
+import { HeroesService } from '../../services/heroes.service';
+
+
+//Importamos la ruta
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent implements OnInit {
+
+  heroes:any[]=[];
+  termino:string='';
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private _heroesService:HeroesService,
+              private _router:Router
+              ) {
+               }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+       this.termino= params['termino'];
+      this.heroes=this._heroesService.buscarHeroes(params['termino']);
+      console.log(this.heroes);
+    });
+  }
+  verHeroe(idx:number){
+    this._router.navigate(['/heroe',idx])
+    console.log(idx);
+  }
+
+}
