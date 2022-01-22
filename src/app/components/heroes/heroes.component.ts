4f090f131c92403f1fdd8309f35b248aeaee29f1
Jesus Garcia <yisusgarcia1002@gmail.com>
2022-01-21 22:56:27 -0500

    Se agregaron todos los heroes recoridos con un *NgFor

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { HeroesService , Heroe} from '../../services/heroes.service';
+
+//Importamos la ruta
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styles: [
-  ]
+  templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
+  
+  //Propiedades
+  heroes:Heroe[] = [];
 
-  constructor() { }
+  constructor( private _heroesService:HeroesService,
+    private router:Router
+    ) { 
 
+
+    console.log('Constructor <- ' );
+
+  }
+  
   ngOnInit(): void {
+    this.heroes=this._heroesService.getHeroers();
+    console.log(this.heroes);
+
+  }
+  verHeroe(idx:number){
+    this.router.navigate(['/heroe',idx])
+    console.log(idx);
   }
 
 }
